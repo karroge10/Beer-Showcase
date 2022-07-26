d4fe6a304ca5497cdb1d3698beb4ab468dbcd8aa
Egor <karroge10@gmail.com>
2022-07-26 10:36:21 +0300

    searchform update

--- components/404.js
+++ components/404.js
@@ -4,7 +4,7 @@ import SearchForm from './SearchForm';
 const Custom404 = () => {
     return (
         <MainContainer pageName="Craft Beer Delivery">
-            <SearchForm />
+            <SearchForm flag={'no-search'}/>
             <div className="error-message">
                 <h1>There has been an error.</h1>
                 <h1>No items were found on this page.</h1>
